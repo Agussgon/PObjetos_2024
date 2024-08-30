24f9cc2b9faea6b628259e7c3ebd603a8415fce5
Agussgon <agusquerencio@gmail.com>
2024-08-30 11:57:32 -0300

    clase de arrays y scanner del paquete utils

--- src/Main.java
+++ src/Main.java
@@ -7,130 +7,76 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
         //scanner
-        countSConsole();
-        Date hoy= new Date();
-       int  anioActual= hoy.getYear();
-        System.out.println(anioActual); //124
-
-        LocalDate fechaActual = LocalDate.now();
-        LocalDate fechaNacimiento = LocalDate.of(1990, 8, 15);
-        System.out.println("Año actual "+fechaActual.getYear() );
+    scannerDatosPersonales();
 
 
-        compareStrings("hola","hola");
+        //arrays
 
-        Integer num1=20;
-        Integer num2=1;
+        System.out.println(" creando array sin ordenar");
+        String[] comidas= new String[3];   // tiene tres elementos
+        comidas[0]= "Pollo con limón";
+        comidas[1]="a";
+        comidas[2]="jamón de pollo con papas";
 
+        for (int i = 0; i < comidas.length ; i++) {
 
-        compareInteger(2,1,110.2);
+            System.out.println(comidas[i]);
 
-
-        countS("sasa asa");
-
-        System.out.println( getAnioNac(33));
-
-        /*
-        Crea un arrays de cuatro películas  e imprimirlas por orden alfabetico.*/
-
-        String[] pelis= new String[]{"Alien", "E.T","Batman","Up"};
-        Arrays.sort(pelis);
-        for (int i = 0; i < pelis.length ; i++) {
-            System.out.println(pelis[i]);
         }
 
-        /*Crea un nuevo array que contenga la lista de películas en mayúscula.*/
-        String[] pelisMayus= new String[4];
-        for (int i = 0; i < pelis.length; i++) {
-          pelisMayus[i]=pelis[i].toUpperCase();
-            System.out.println(pelisMayus[i]);
-        }
-
-        /*Crea una variable que guardará la primera película que comience con “L”,
-        debe recorrer el array buscando el primer resultado.
-         */
+        Integer[] numeros= new Integer[3];
+        numeros[2]=123;
 
 
+        System.out.println("recorriendo array de números con while");
+        int contador=0;
 
+        while(contador < numeros.length || contador < 3 ){
 
-    }
-
-
-    /* Crea una función que reciba dos cadenas de texto. Debe comparar si son iguales y
-    retornar un booleano además de imprimir un mensaje con el resultado.
-     */
-
-    public static boolean compareStrings(String cadena1, String cadena2){
-        boolean resultado=cadena1.equals(cadena2);
-        System.out.println(resultado);
-        return resultado;
-
-    }
-
-    /*      Definir dos variables Integer y asignarles un valor a cada una,
-    crea una función que determine
-     si el primero es mayor que el producto del segundo y un porcentaje dado
-     */
-
-    public static Integer compareInteger(Integer numA, Integer numB,Double porcentaje){
-        Integer resultado=numA.compareTo((int)(numB* porcentaje));// No hay conversión directa
-        if (resultado >= 0){
-            System.out.println("El primero es mayor ");
-        }else{
-            System.out.println("El primero es menor ");
-        }
-        return resultado;
-    }
-
-    /*Escribí una función que reciba una cadena de texto y cuente la cantidad de “s”.
-     Retorna la cantidad encontrada.
-     */
-
-    public static Integer countS(String texto){
-        Integer contador=0;
-        for (int i = 0; i <texto.length() ; i++) {
-        if(texto.charAt(i) == 's'){
+            System.out.println(numeros[contador]+ " ¿Dónde está Fede?" );
             contador ++;
-
-        };
+            if( contador == 3){
+                System.out.println(" Acá estoy");
+            }
         }
-        System.out.println(contador);
-        return  contador;
+        // simplemente lo ordenamos
+        System.out.println("Ahora ordenamos alfabéticamente el array comidas");
+      Arrays.sort(comidas);
+        for (int i = 0; i < comidas.length ; i++) {
+            System.out.println(comidas[i]);
+        }
+/// recorro el array para imprimir la inicial de cada elemento
+        for (int i = 0; i < comidas.length ; i++) {
+            comidas[i].charAt(0);
+            System.out.println("Imprimo la inicial "+ comidas[i].charAt(0));
+        }
+
+
+        System.out.println(" Ejercicio con copia del Array");
+        // minusc lower case
+        String[] copiaComidas= new String[3];
+
+        for (int i = 0; i < comidas.length ; i++) {
+            copiaComidas[i]= comidas[i].toUpperCase()  ;
+            System.out.println(copiaComidas[i]);
+        }
+
+
+
     }
 
-    /*Crea una función que reciba una edad y determine el año de nacimiento*/
-
-    public static int getAnioNac(int edad){
-        int anioActual= LocalDate.now().getYear();
-        return anioActual - edad;
-
-    }
-
-    /*Resolver el ejercicio 7 de la primera parte( switch) , el 4 y 6 de esta 2°
-    implementando la clase Scanner.
-     */
-
-    public static void countSConsole(){
+    public static void scannerDatosPersonales(){
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Ingrese el texto para contabilizar las s");
-        String texto= scanner.nextLine();
-        countS(texto);
-        // podríamos pedir que nos envien varios datos y almacenarlos en != variables utilizando un separador
-        String[] palabras=texto.split(" ");
-        String palabra1= palabras[0];
-        System.out.println(palabra1);
+        System.out.println("Ingrese su nombre y edad: ");
+        String nombreRecibido = scanner.nextLine();
+        Integer edadRecibida= scanner.nextInt();
+        System.out.println("usted ingreso la siguiente información "+ '\n'
+                + nombreRecibido
+                + '\n'+ " la clase es " + nombreRecibido.getClass()
+                + '\n'+ "la edad ingresada es" + edadRecibida + '\n'+
+                " la clase del objeto es "+ edadRecibida.getClass() );
 
-    }
-
-
-    // casteo
-
-    public static void calcularFaltas(int encuentros, double porcentaje){
-        int a= (int) (encuentros*porcentaje);
-        int faltas= encuentros - a;
-        System.out.println(faltas);
     }
 
 
