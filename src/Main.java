a7b46584fcb4af1c56cb98c453c392bbb31c2005
Agussgon <agusquerencio@gmail.com>
2024-09-05 11:54:43 -0300

    clase & objeto- | constructor & métodos

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import entidades.Producto;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Arrays;
@@ -5,75 +7,26 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        //scanner
-    scannerDatosPersonales();
-        //arrays
-        System.out.println(" creando array sin ordenar");
-        String[] comidas= new String[3];   // tiene tres elementos
-        comidas[0]= "Pollo con limón";
-        comidas[1]="a";
-        comidas[2]="jamón de pollo con papas";
 
-        for (int i = 0; i < comidas.length ; i++) {
+        //instanciamos el objeto
+        Producto pelotaModelo1= new Producto("pelota modelo 2024",25000.0,
+                50000.0,20);
+        System.out.println(" este es el objeto creado con el constructor personalizado"+
+                '\n'+ pelotaModelo1+
+                '\n'+ pelotaModelo1.informarStock()+
+                '\n'+ pelotaModelo1.stockDisponible()+
+                '\n'+pelotaModelo1.calcularGananciaObtenida());
 
-            System.out.println(comidas[i]);
+        Producto pelotaModelo2= new Producto();
+        System.out.println("este lo creamos con el que viene por defecto implicito" +pelotaModelo2);
 
-        }
-
-        Integer[] numeros= new Integer[3];
-        numeros[2]=123;
-
-
-        System.out.println("recorriendo array de números con while");
-        int contador=0;
-
-        while(contador < numeros.length || contador < 3 ){
-
-            System.out.println(numeros[contador]+ " ¿Dónde está Fede?" );
-            contador ++;
-            if( contador == 3){
-                System.out.println(" Acá estoy");
-            }
-        }
-        // simplemente lo ordenamos
-        System.out.println("Ahora ordenamos alfabéticamente el array comidas");
-      Arrays.sort(comidas);
-        for (int i = 0; i < comidas.length ; i++) {
-            System.out.println(comidas[i]);
-        }
-/// recorro el array para imprimir la inicial de cada elemento
-        for (int i = 0; i < comidas.length ; i++) {
-            comidas[i].charAt(0);
-            System.out.println("Imprimo la inicial "+ comidas[i].charAt(0));
-        }
-
-
-        System.out.println(" Ejercicio con copia del Array");
-        // minusc lower case
-        String[] copiaComidas= new String[3];
-
-        for (int i = 0; i < comidas.length ; i++) {
-            copiaComidas[i]= comidas[i].toUpperCase()  ;
-            System.out.println(copiaComidas[i]);
-        }
-
-
-
+        Producto pelotaModelo3= new Producto("tercer modelo",30 );
+        System.out.println("tercer modelo "+ pelotaModelo3);
     }
 
-    public static void scannerDatosPersonales(){
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("Ingrese su nombre y edad: ");
-        String nombreRecibido = scanner.nextLine();
-        Integer edadRecibida= scanner.nextInt();
-        System.out.println("usted ingreso la siguiente información "+ '\n'
-                + nombreRecibido
-                + '\n'+ " la clase es " + nombreRecibido.getClass()
-                + '\n'+ "la edad ingresada es" + edadRecibida + '\n'+
-                " la clase del objeto es "+ edadRecibida.getClass() );
 
-    }
 
 
 }
---
+++ src/entidades/Producto.java
@@ -0,0 +1,69 @@
+package entidades;
+
+/*Necesitamos diseñar un sistema que permita a un vendedor consultar  por un producto.
+ Los productos tienen nombre, precio de compra, precio de venta y stock disponibles.
+
+ Se espera que los productos puedan informar si hay stock disponible
+ y cuál es la ganancia obtenida por cada venta, según los precios disponibles.
+ */
+public class Producto {
+
+    //atributos
+private String nombre;
+private Double precioCompra ;
+private Double precioVenta;
+private Integer stock;
+
+// constructor explicito
+public Producto(String nombreRecibido, Double precioCompraRecibido,
+                Double precioVentaR, Integer stockR  ){
+    this.nombre=nombreRecibido  ;
+    this.precioCompra=precioCompraRecibido;
+    this.precioVenta=  precioVentaR;
+    this.stock=stockR;
+
+}
+// el que no se ve es implicito
+public Producto(){}
+
+//public Producto
+    public Producto(String nombreR, Integer stock){
+    this.nombre= nombreR;
+    this.stock= stock;
+    }
+
+// métodos
+  public  Boolean stockDisponible(){
+      return stock > 0;
+  }
+
+  public String informarStock(){
+    return  "el stock disponible es "+ this.stock  ;
+    }
+
+  public Double calcularGananciaObtenida(){
+      return this.precioVenta - this.precioCompra ;
+  }
+
+
+// Métodos para mantener el encapsulamiento
+// armamos un método para el único vendedor que esta habilitado para modificar el stock"
+  public void setStock(Integer nuevoStock){
+    this.stock= nuevoStock;
+  }
+
+  public Integer getStock(){
+    return this.stock;
+  }
+
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precioCompra=" + precioCompra +
+                ", precioVenta=" + precioVenta +
+                ", stock=" + stock +
+                '}';
+    }
+}
