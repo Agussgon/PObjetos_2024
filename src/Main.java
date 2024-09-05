7ede8acbc3e42016c559fd96d8d781440802e73f
Agussgon <agusquerencio@gmail.com>
2024-09-05 16:29:34 -0300

    arrays y scanner

--- src/Main.java
+++ src/Main.java
@@ -8,169 +8,60 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-        Object obj = "123";
-        //Integer numero = (Integer) obj; // Intento de casteo inválido classcastException
-
-        if (obj instanceof String) {
-            String str = (String) obj; //casteo
-            Integer numero = Integer.parseInt(str); // Parseo de String a Integer
-            System.out.println("Número: " + numero);
-        }
-
-
-
-        String cadena="1234" ;
-        Integer num= Integer.parseInt(cadena);
-        Double numDouble= Double.parseDouble(cadena);
-        String num2= Integer.toString(num);
-
-
-        LocalDate fechaActual = LocalDate.now();
-        LocalDate fechaNacimiento = LocalDate.of(1990, 8, 15);
-        System.out.println("Año actual "+fechaActual.getYear() );
-       // System.out.println(fechaActual.getYear() - fechaNacimiento.getYear());
-
-
-        //of pattern
-        DateTimeFormatter fechaHoraPersonalizada= DateTimeFormatter.ofPattern("yyy MM dd");
-        String fechaFormateada=fechaHoraPersonalizada.format(fechaNacimiento);
-        System.out.println(fechaFormateada);
-
-        Date fecha=new Date(100,2,20);
-        System.out.println(fecha.toString());
-        Date hoy= new Date();
-        int  anioActual= hoy.getYear();
-        System.out.println(anioActual); //124
-
-        Float decimal=2.5f;
-        Double a= 2.5;
-        a.equals(decimal);
-
-
-
-        String saludo= null;
-        //saludo="";
-
-        if (saludo == null)
-        {
-            System.out.println("Cadena con valor nulo");
-        }else
-        {System.out.println("¿Qué retorna?" + saludo.length());
-        }
-
-        compareStrings("hola","hola");
-
-        Integer num1=20;
-        Integer num2_2=1;
-
-        compareInteger(2,1,110.2);
-
-        countS("sasa asa");
-
-        System.out.println( getAnioNac(33));
-
-        /*
-        Crea un arrays de cuatro películas  e imprimirlas por orden alfabetico.*/
-
-        String[] pelis= new String[]{"Alien", "E.T","Batman","Up"};
-        Arrays.sort(pelis);
-        for (int i = 0; i < pelis.length ; i++) {
-            System.out.println(pelis[i]);
-        }
-
-        /*Crea un nuevo array que contenga la lista de películas en mayúscula.*/
-        String[] pelisMayus= new String[4];
-        for (int i = 0; i < pelis.length; i++) {
-          pelisMayus[i]=pelis[i].toUpperCase();
-            System.out.println(pelisMayus[i]);
-        }
-
-        //scanner
-        countSConsole();
-
+       // arrayScannerUnaLinea();
+       arrayScannerPelisSeparados();
 
     }
 
+/*Crea un array de cuatro películas  e imprimirlas por orden alfabético.
+Crea un nuevo array que contenga la lista de películas en mayúscula. (main)
+Resolver los ejercicios 1 y 2 implementando dos funciones que utilicen la clase Scanner.
 
-    /* Crea una función que reciba dos cadenas de texto. Debe comparar si son iguales y
-    retornar un booleano además de imprimir un mensaje con el resultado.
-     */
+Crea una variable que guardará la primera película que comience con “L”,
+se debe recorrer el array buscando el primer resultado.
 
-    public static boolean compareStrings(String cadena1, String cadena2){
-        boolean resultado=cadena1.equals(cadena2);
-        System.out.println(resultado);
-        return resultado;
+Necesitamos un programa que reciba dos cadenas de texto y
+que  cada una contenga dos películas separadas por una coma.
+El método debe retornar la que tenga mayor cantidad de caracteres.
+Debe generar un array de cuatro elementos.
 
-    }
+* */
 
-    /*      Definir dos variables Integer y asignarles un valor a cada una,
-    crea una función que determine
-     si el primero es mayor que el producto del segundo y un porcentaje dado
-     */
-
-    public static Integer compareInteger(Integer numA, Integer numB,Double porcentaje){
-        Integer resultado=numA.compareTo((int)(numB* porcentaje));// No hay conversión directa
-        if (resultado >= 0){
-            System.out.println("El primero es mayor ");
-        }else{
-            System.out.println("El primero es menor ");
-        }
-        return resultado;
-    }
-
-    /*Escribí una función que reciba una cadena de texto y cuente la cantidad de “s”.
-     Retorna la cantidad encontrada.
-     */
-
-    public static Integer countS(String texto){
-        Integer contador=0;
-        for (int i = 0; i <texto.length() ; i++) {
-        if(texto.charAt(i) == 's'){
-            contador ++;
-
-        };
-        }
-        System.out.println(contador);
-        return  contador;
-    }
-
-    /*Crea una función que reciba una edad y determine el año de nacimiento*/
-
-    public static int getAnioNac(int edad){
-        int anioActual= LocalDate.now().getYear();
-        return anioActual - edad;
-
-    }
-
-    /*Resolver el ejercicio 7 de la primera parte( switch) , el 4 y 6 de esta 2°
-    implementando la clase Scanner.
-     */
-
-    public static void countSConsole(){
+    public static void arrayScannerUnaLinea(){
+       // String[] nuevasPelis= new String[4];
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Ingrese el texto para contabilizar las s");
-        String texto= scanner.nextLine();
-        countS(texto);
-        // podríamos pedir que nos envien varios datos y almacenarlos en != variables utilizando un separador
-        String[] palabras=texto.split(" ");
-        String palabra1= palabras[0];
-        System.out.println(palabra1);
-
+        System.out.println("ingresa las cuatro pelis separadas por coma");
+        String pelis= scanner.nextLine();
+        String[] pelisAgrupadas= pelis.split(","); // este método crea el nuevo array de 4 elementos x nostros
+        for (int i = 0; i < pelisAgrupadas.length; i++) {
+            System.out.println(pelisAgrupadas[i]);
+        }
     }
 
+    public static void arrayScannerPelisSeparados(){
+        String[] nuevasPelis= new String[3];
+        String peliL;
 
-
-
-
-
-    // casteo
-
-    public static void calcularFaltas(int encuentros, double porcentaje){
-        int a= (int) (encuentros*porcentaje);
-        int faltas= encuentros - a;
-        System.out.println(faltas);
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("ingresa la peli 1 ");
+        String pelis1= scanner.nextLine();
+        nuevasPelis[0]=pelis1;
+        //System.out.println("ingresa la peli 2");
+        String pelis2= scanner.nextLine();
+        nuevasPelis[1]=pelis2;
+      //  System.out.println("ingresa la peli 3");
+        String pelis3= scanner.nextLine();
+        nuevasPelis[2]=pelis3;
+        Arrays.sort(nuevasPelis);
+        for (int i = 0; i < nuevasPelis.length; i++) {
+            if(nuevasPelis[i].toLowerCase().charAt(0) == 'l'){
+                peliL=nuevasPelis[i];
+                System.out.println("esta es la peli que inicia con L" + "\n" +peliL);
+            }
+            System.out.println(nuevasPelis[i].toUpperCase());
+        }
     }
+
 
 
 }
