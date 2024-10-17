929e554f6276d95b87076c9bfaac3d0a933ddeef
Agussgon <agusquerencio@gmail.com>
2024-10-17 07:25:37 -0300

    m formas del ©meodo extraer y uso de n° negativos

--- src/CuentaCorriente.java
+++ src/CuentaCorriente.java
@@ -1,45 +1,74 @@
-public class CuentaCorriente extends Cuenta{
+public class CuentaCorriente extends Cuenta {
 
-    private static Double limiteMontoDescubierto= 50000.0;
+    private static Double limiteMontoDescubierto = 50000.0;
 
-    private Double montoDisponible= limiteMontoDescubierto;
+    private Double montoDisponible = limiteMontoDescubierto;
 
     public CuentaCorriente(Double saldo, Cliente clienteAsociado) {
         super(saldo, clienteAsociado);
 
     }
 
-    @Override
-    public void depositar(Double monto){
-        if (monto <= 0 ){
-            System.out.println("ingrese un monto válido");
-        }else{
-            //this.saldo= this.saldo + monto;
-            Double nuevoSaldo= this.getSaldo()+monto;
-            this.setSaldo(nuevoSaldo);
-        }
 
+    @Override
+    public void depositar(Double monto) {
+        if (monto <= 0) {
+            System.out.println("ingrese un monto válido");
+        } else {
+            if (!montoDisponible.equals(limiteMontoDescubierto)) {
+                Double diferencia = limiteMontoDescubierto - montoDisponible;
+                this.montoDisponible += diferencia;
+                // restamos al deposito lo utilizado
+                monto -= diferencia;
+                // ahora guardamos lo restante en saldo que estaba en cero...
+                this.setSaldo(monto);
+                System.out.println("saldo actual después de depositar" + this.getSaldo());
+
+            } else {
+                Double nuevoSaldo = this.getSaldo() + monto;
+                this.setSaldo(nuevoSaldo);
+                System.out.println("saldo actual después de depositar " + this.getSaldo());
+            }
+        }
     }
 
     @Override
-    public Double extraer(Double monto){
-       // primero vemos si el saldo alcanza
-        if(monto <= this.getSaldo()){
-            setSaldo(this.getSaldo()-monto);
-            System.out.println("saldo actual después de extraer "+ this.getSaldo());
+    public Double extraer(Double monto) {
+        // primero vemos si el saldo alcanza
+        if (monto <= this.getSaldo()) {
+            setSaldo(this.getSaldo() - monto);
+            System.out.println("saldo actual después de extraer " + this.getSaldo());
             return monto;
-        } else if( monto <= (this.getSaldo()+ this.montoDisponible)){
+        } else if (monto <= (this.getSaldo() + this.montoDisponible)) {
             // sino sumamos el descubierto
             setSaldo(0.0);
-            Double nuevoMontoDisponible= monto-this.getSaldo();
-            this.montoDisponible= this.montoDisponible - nuevoMontoDisponible;
-            System.out.println("monto disponible actual después de extraer "+ this.montoDisponible);
+            Double nuevoMontoDisponible = monto - this.getSaldo();
+            this.montoDisponible = this.montoDisponible - nuevoMontoDisponible;
+            System.out.println("monto disponible actual después de extraer " + this.montoDisponible);
             return monto;
 
         }
         // sino error
         System.out.println("supera el límite");
-       return null;
+        return null;
+
+    }
+
+    // otra opción sin el atributo montoDisponible
+    // el método depositar en este caso seria igual a la última opción del actual...
+    public Double extraer2(Double monto) {
+        if (monto <= this.getSaldo()) {
+            setSaldo(this.getSaldo() - monto);
+            System.out.println("saldo actual después de extraer " + this.getSaldo());
+            return monto;
+        } else if (monto - this.getSaldo() <= limiteMontoDescubierto) {
+            setSaldo(this.getSaldo() - monto);
+            System.out.println("Al utilizar el descubierto el saldo quedo en " + this.getSaldo());
+            return monto;
+
+        }
+        System.out.println("no hay saldo suficiente");
+        return null;
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,34 @@ public class Main {
     public static void main(String[] args) {
 
 
+
         CuentaCorriente cuentaCorriente= new CuentaCorriente(5000.0,new Cliente());
        // System.out.println( cuentaCorriente.extraer(10000.0) );
+   /*
 
+    System.out.println("utilizando el atributo monto acumulado");
         cuentaCorriente.extraer(2000.0);
         cuentaCorriente.extraer(10000.0);
         cuentaCorriente.extraer(500000.0);
+
+*/
+
+        System.out.println("sin utilizar el atributo monto acumulado");
+        cuentaCorriente.extraer2(5000.0);
+        cuentaCorriente.extraer2(100000.0);
+        cuentaCorriente.extraer2(50000.0);
+
+        cuentaCorriente.depositar(80000.0);
+
+
+        // n° negativos
+        double n= 23;
+        if( 20 > -n ){
+            System.out.println("20 es más grande");
+        }
+        if(n > - cuentaCorriente.getSaldo()){
+            System.out.println("23 es más grande");
+        }
 
 
 
